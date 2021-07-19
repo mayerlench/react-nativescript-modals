6e3ba93d9d208d1083ce0f535450af0b528f1429
Mayer Lench <mayerlench1314@gmail.com>
2021-07-19 17:41:54 -0400

    removes unused components

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,18 +1,10 @@
 import * as React from "react";
-import { RouteProp } from '@react-navigation/core';
-import { FrameNavigationProp } from "react-nativescript-navigation";
 import { StyleSheet } from "react-nativescript";
-import { MainStackParamList } from "./NavigationParamList";
 import Modal from "./Modal";
 import BottomSheet from "./BottomSheet";
 import { SnackBarTest } from "./SnackBar";
 
-type HomeScreenProps = {
-    route: RouteProp<MainStackParamList, "Home">,
-    navigation: FrameNavigationProp<MainStackParamList, "Home">,
-}
-
-export function HomeScreen({ navigation }: HomeScreenProps) {
+export function HomeScreen() {
     const containerRef = React.useRef(null)
     return (
         <flexboxLayout style={styles.container} ref={containerRef}>
