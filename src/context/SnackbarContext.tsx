b65d4da1454ba5a709ffeb13808f1ba077bd0073
Mayer Lench <mayerlench1314@gmail.com>
2021-07-29 16:59:47 -0400

    updated

--- src/context/SnackbarContext.tsx
+++ src/context/SnackbarContext.tsx
@@ -1,15 +1,18 @@
 import React, { createContext, useRef } from 'react'
 import { SnackBar, SnackBarOptions } from '@nativescript-community/ui-material-snackbar';
 import { Frame, StackLayout } from '@nativescript/core';
+import { getRootView } from '@nativescript/core/application';
 
 export interface SnackbarContextValue {
     simple: (message: string, textColor?: string, backgroundColor?: string, maxLines?: number, isRTL?: boolean) => Promise<any>
     action: (options: SnackBarOptions) => Promise<any>
+    dismiss: (options: SnackBarOptions) => Promise<any>
 }
 
 const SnackbarContext = createContext<SnackbarContextValue>({
     simple: () => Promise.resolve(),
     action: () => Promise.resolve(),
+    dismiss: () => Promise.resolve(),
 })
 
 export const SnackbarProvider = ({ children }) => {
@@ -17,9 +20,9 @@ export const SnackbarProvider = ({ children }) => {
     const snackbar = new SnackBar();
 
     const simple = (message: string, textColor?: string, backgroundColor?: string, maxLines?: number, isRTL?: boolean) => {
-        const view = ref.current.nativeView as StackLayout
+        const view = getRootView() as StackLayout
         const frame = view.getChildAt(0) as Frame
-        const page = frame.currentEntry ? frame.currentEntry.create() : null
+        const page = frame.currentEntry ? frame.currentEntry.create() : view
 
         return snackbar.action({
             message,
@@ -30,12 +33,14 @@ export const SnackbarProvider = ({ children }) => {
             view: page
         })
     }
+
     const action = (options: SnackBarOptions) => snackbar.action({ ...options, view: ref.current.nativeView })
     return (
         <SnackbarContext.Provider
             value={{
                 simple,
-                action
+                action,
+                dismiss: snackbar.dismiss
             }}
         >
             <stackLayout ref={ref}>
